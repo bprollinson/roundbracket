afe39564937d77a47674b2294bfd3fc52998393f
Brendan Rollinson <brendan.rollinson@gmail.com>
2024-11-14 20:16:01 -0500

    Implementing equality for boolean operator tokens

--- src/main/roundbracket/token/OrToken.java
+++ src/main/roundbracket/token/OrToken.java
@@ -2,4 +2,8 @@ package roundbracket.token;
 
 public class OrToken implements Token
 {
+    public boolean equals(Object other)
+    {
+        return this.getClass().equals(other.getClass());
+    }
 }
